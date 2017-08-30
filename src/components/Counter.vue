ec717139b967e7e9fa3d4f1ed1ddfef8a097cec9
Syeed Redom <=>
2017-08-30 12:59:50 -0400

    Initial Commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<h2>Smile  :) | You are on Count {{ count }} |</h2>
+		<p>
+			<button @click="count += 1">Increment</button>
+		</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				count: 1
+			}
+		}
+	}
+</script>
